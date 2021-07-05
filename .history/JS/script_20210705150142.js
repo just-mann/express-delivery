83f6eb95995c2bd3bad06933e5a6d03e3dc6efe8
Justin <justinkollie890@yahoo.com>
2021-07-05 16:01:44 -0400

    Edited moble view of navbar and hero section

---
+++ .history/JS/script_20210705150142.js
@@ -0,0 +1,36 @@
+// alert('It works');
+
+
+// Preloader
+$(() => {
+
+    // const scrollY = window.scrollY;
+
+    $('#preloader').show();
+    // $('.page__up__arrow').addClass('d-none');
+    
+    $('.menuBars').on('click', () => {
+        $('.side__nav__links').show('slow');
+    })
+
+    $('.closeBtn').on('click', () => {
+        $('.side__nav__links').hide('slow');
+    })
+
+    
+    $(window).on('load', () => {
+        $('#preloader').delay(1000).fadeOut('slow', () => {
+            $(this).remove();
+            $('#preloader').removeClass('d-block');
+            $('#preloader').addClass('d-none');
+        });
+    });
+
+    $('.page__up__arrow').on('click', () => {
+        $('html, body').animate({
+            scrollTop: $('#top').offset().top
+        }, 300)
+    })
+
+    
+});
