5027cbc9aa41a128c0682d4d6fda362136fd3147
Justin <justinkollie890@yahoo.com>
2021-07-07 12:56:30 -0400

    Changes made

---
+++ .history/JS/script_20210707101534.js
@@ -0,0 +1,57 @@
+// alert('It works');
+
+
+// Preloader
+$(() => {
+
+    // const scrollY = window.scrollY;
+
+    $('#preloader').show();
+    // $('.page__up__arrow').addClass('d-none');
+    
+    // Display side nav when menuBars is clicked
+    $('.menuBars').on('click', () => {
+        $('.side__nav__links').show('slow');
+    });
+
+    // Close side nav when closeBtn is clicked
+    $('.closeBtn').on('click', () => {
+        $('.side__nav__links').hide('slow');
+    });
+
+    // Hide side nav when its links are clicked
+    $('.side__nav__links a').on('click', () => {
+        $('.side__nav__links').hide('slow');
+    });
+
+    // When the page loads remove preloader within 1.1sec
+    $(window).on('load', () => {
+        $('#preloader').delay(1200).fadeOut('slow', () => {
+            $(this).remove();
+            // $('#preloader').removeClass('d-block');
+            // $('#preloader').addClass('d-none');
+            $('#preloader').hide();
+        });
+    });
+
+    // When user scrolls down, hide side nav and display
+        // up arrow.
+    $(window).on('scroll', () => {
+        if (window.scrollY > 5) {
+            $('.side__nav__links').hide('slow');
+            $('.page__up__arrow').css('display', 'block');
+        } else {
+            $('.page__up__arrow').css('display', 'none');
+        }
+    });
+    
+
+    // Scroll up to top of page when up arrow is clicked
+    $('.page__up__arrow').on('click', () => {
+        $('html, body').animate({
+            scrollTop: $('#top').offset().top
+        }, 300)
+    });
+
+    
+});
